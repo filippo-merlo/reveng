4fc6c5920f428d4673e646e90a5f23c19a7e4a5c
raarghal <rarghal@seas.upenn.edu>
2026-05-03 04:12:39 -0400

    top k decoded grid analysis

--- src/reveng/experiments/generate_dataset_isodifficulty.py
+++ src/reveng/experiments/generate_dataset_isodifficulty.py
@@ -3,14 +3,23 @@ This code applies iso-difficulty transforms [ReflectEnv, RotateEnv, TransposeEnv
 to the existing dataset baseline_grids
 
 """
-import pickle, sys, os
-sys.path.append(os.path.join('C:/users/hchen/dropbox/reveng', "src"))
-import reveng
-from reveng.environment_generator.env_transformations import ReflectEnv, RotateEnv, TransposeEnv, StartGoalSwap
-import matplotlib.pyplot as plt
 
-dataset_path = 'C:/users/hchen/dropbox/reveng/src/reveng/experiments/datasets/baseline_grids.pkl'
-output_path = 'C:/users/hchen/dropbox/reveng/src/reveng/experiments/datasets/isodifficulty_grids.pkl'
+import os
+import pickle
+import sys
+
+sys.path.append(os.path.join("C:/users/hchen/dropbox/reveng", "src"))
+from reveng.environment_generator.env_transformations import (
+    ReflectEnv,
+    RotateEnv,
+    StartGoalSwap,
+    TransposeEnv,
+)
+
+dataset_path = (
+    "C:/users/hchen/dropbox/reveng/src/reveng/experiments/datasets/baseline_grids.pkl"
+)
+output_path = "C:/users/hchen/dropbox/reveng/src/reveng/experiments/datasets/isodifficulty_grids.pkl"
 
 print("\n1. Loading datasets...")
 print(f"   Dataset: {dataset_path}")
@@ -21,11 +30,10 @@ print(f"   Loaded {len(grids_dataset)} environments from dataset")
 iso_dict = {}
 for key in grids_dataset.keys():
     env = grids_dataset[key]
-    iso_dict[key+'_RotateEnv'] = RotateEnv().apply(env)
-    iso_dict[key+'_ReflectEnv'] = ReflectEnv().apply(env)
-    iso_dict[key+'_TransposeEnv'] = TransposeEnv().apply(env)
-    iso_dict[key+'_StartGoalSwap'] = StartGoalSwap().apply(env)
+    iso_dict[key + "_RotateEnv"] = RotateEnv().apply(env)
+    iso_dict[key + "_ReflectEnv"] = ReflectEnv().apply(env)
+    iso_dict[key + "_TransposeEnv"] = TransposeEnv().apply(env)
+    iso_dict[key + "_StartGoalSwap"] = StartGoalSwap().apply(env)
 
- with open(output_path, "wb") as f:
-        pickle.dump(iso_dict, f)
-
+with open(output_path, "wb") as f:
+    pickle.dump(iso_dict, f)
